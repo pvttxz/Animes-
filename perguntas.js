916005bd0d426f071bb00fd874f39cbc7b15589f
paolinhaa <paolavictoriademeira3105@gmail.com>
2024-11-27 11:15:57 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,24 @@
+criaCartao(
+    'One piece',
+    'Quais sao o tipos de akuma no mi?',
+    'Paramecia, zoan e logia'
+)
+
+criaCartao(
+    'Mugiwaras',
+    'Quem é Nico Robin?',
+    'Ela foi introduzida com uma inimiga do bando fazendo parte da Baroque Works, e após os eventos de Alabasta decide se unir ao grupo.'
+)
+
+criaCartao(
+    'Attack on titan',
+    'Os titãs sempre existiram?',
+    'Na realidade, eles são humanos transformados conhecidos como Súditos de Ymir e existem há quase 2.000 anos.'
+)
+
+criaCartao(
+    'Jujutsu',
+    'qual é a habilidade eepecial de satoro gojo?',
+    'Telecinese (Pode induzir uma força invisível para esmagar alvos) Manipulação do Sono (Pode colocar as pessoas para dormir com um toque) Reação Instintiva (Graças ao Seis Olhos, ele pode subconscientemente ajustar seu escudo infinito e técnicas de regeneração conforme necessário)'
+
+)
